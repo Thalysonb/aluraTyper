6fd2ebb0519823f4dbb60d485c7894b9b70607d0
Thalyson <thalyson98brandao@gmail.com>
2019-02-26 16:20:39 -0300

    Criando o contador, e a função de reiniciar o jogo

---
+++ public/js/main.js
@@ -0,0 +1,42 @@
+
+var frase = $(".frase").text();
+var numPalavras = frase.split(" ").length;
+
+var tamanhoFrase = $("#tamanho-frase");
+tamanhoFrase.text(numPalavras);
+
+var campo = $(".campo-digitacao");
+
+campo.on('input',function(){
+    var valTexto = campo.val();
+
+    var valTextoSemEspaco = valTexto.replace(/\s+/g,''); //remove os espaços da string
+
+    var qtdCaracter = valTextoSemEspaco.length; //conta caracteres
+    var qtdPalavras = valTexto.split(/\S+/).length - 1; //conta as palavras
+   
+
+    var contadorCaracter = $("#contador-caracter");
+    contadorCaracter.text(qtdCaracter); //altera o valor do contador de caracter
+
+    var contadorPalavra = $("#contador-palavra");
+    contadorPalavra.text(qtdPalavras); //altera o valor do contador de palavras
+});
+
+
+var tempoRestante = $("#tempo-digitacao").text();
+
+campo.one('focus',function(){
+    var cronometroId = setInterval(function(){
+        tempoRestante--;
+        
+        $("#tempo-digitacao").text(tempoRestante);
+
+        if (tempoRestante == 0){
+            clearInterval(cronometroId);
+
+            campo.attr("disabled",true);
+        }
+        
+    },1000);
+});
