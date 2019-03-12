c5213a471ae280ad28bbfc46d582851b45f04af2
Thalyson <thalyson98brandao@gmail.com>
2019-03-12 15:29:46 -0300

    Organizando o cógido em arquivos separados

--- public/js/main.js
+++ public/js/main.js
@@ -89,39 +89,4 @@ function reiniciaJogo() {
     campo.removeClass("borda-vermelha");
 }
 
-function salvaPlacar(){
-    var tabPlacar = $('#placar').find('tbody');
-    var numPalavras = $("#contador-palavra").text();
-    var nome = "Thalyson";
-    var linha = novaLinha(nome,numPalavras);
-
-    linha.find(".botao-remover").click(removeLinha);
-
-    tabPlacar.prepend(linha);    
-}
-
-
-function novaLinha(usuario,palavras) {    
-    var linha = $("<tr>");
-    var colNome = $("<td>").text(usuario);
-    var colNumPalavras = $("<td>").text(palavras);
-    var colRemover = $("<td>");
-    var link = $("<a>").addClass("botao-remover").attr("href","#");
-    var icone = $("<i>").addClass("small").addClass("material-icons").text("delete").attr("id","teste");
-
-    link.append(icone);
-    colRemover.append(link);
-
-    linha.append(colNome);
-    linha.append(colNumPalavras);
-    linha.append(colRemover);
-
-    return linha
-}
-
-
-function removeLinha(event){
-    event.preventDefault();
-    $(this).parent().parent().remove();
-}
 
